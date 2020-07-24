e3103ec89f91684a8835bfef1323bc7953350959
Isaac Gorman <emailme@isaacg.net>
2020-07-24 10:31:36 -0500

    handleChange, handleSubmit on form built

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function From() {
+  const defaultValues = {
+    name: "",
+    pizzaSize: "",
+    pepperoni: false,
+    pineapple: false,
+    mushroom: false,
+    extraCheese: false,
+    specialInstructions: "",
+  };
+
+  const [formState, setFormState] = useState(defaultValues);
+
+  const handleChange = (e) => {
+    const value =
+      e.target.type == "checkbox" ? e.target.checked : e.target.value;
+    setFormState({ ...formState, [e.target.name]: value });
+    console.log("formState: ", formState);
+  };
+
   return (
     <div>
       <h2>Form Page</h2>
